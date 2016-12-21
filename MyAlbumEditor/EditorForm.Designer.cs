4630eca568ee564e76f42a6ecbf7a7828532d53b
fabio.priamo <fabio.priamo@PCTESTE1>
2016-12-21 14:16:17 -0200

    Add project files.

---
+++ MyAlbumEditor/EditorForm.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace MyAlbumEditor
+{
+    partial class EditorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpAlbums = new System.Windows.Forms.GroupBox();
+            this.btnAlbumProps = new System.Windows.Forms.Button();
+            this.grpPhotos = new System.Windows.Forms.GroupBox();
+            this.btnMoveUp = new System.Windows.Forms.Button();
+            this.btnMoveDown = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnPhotoProps = new System.Windows.Forms.Button();
+            this.lstPhotos = new System.Windows.Forms.ListBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.comboAlbums = new System.Windows.Forms.ComboBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.grpAlbums.SuspendLayout();
+            this.grpPhotos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpAlbums
+            // 
+            this.grpAlbums.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grpAlbums.Controls.Add(this.btnBrowse);
+            this.grpAlbums.Controls.Add(this.comboAlbums);
+            this.grpAlbums.Controls.Add(this.btnAlbumProps);
+            this.grpAlbums.Location = new System.Drawing.Point(13, 13);
+            this.grpAlbums.Name = "grpAlbums";
+            this.grpAlbums.Size = new System.Drawing.Size(359, 45);
+            this.grpAlbums.TabIndex = 0;
+            this.grpAlbums.TabStop = false;
+            this.grpAlbums.Text = "&Albums";
+            // 
+            // btnAlbumProps
+            // 
+            this.btnAlbumProps.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAlbumProps.Enabled = false;
+            this.btnAlbumProps.Location = new System.Drawing.Point(272, 16);
+            this.btnAlbumProps.Name = "btnAlbumProps";
+            this.btnAlbumProps.Size = new System.Drawing.Size(75, 23);
+            this.btnAlbumProps.TabIndex = 1;
+            this.btnAlbumProps.Text = "Propertie&s";
+            this.btnAlbumProps.UseVisualStyleBackColor = true;
+            this.btnAlbumProps.Click += new System.EventHandler(this.btnAlbumProps_Click);
+            // 
+            // grpPhotos
+            // 
+            this.grpPhotos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grpPhotos.Controls.Add(this.btnMoveUp);
+            this.grpPhotos.Controls.Add(this.btnMoveDown);
+            this.grpPhotos.Controls.Add(this.btnRemove);
+            this.grpPhotos.Controls.Add(this.btnPhotoProps);
+            this.grpPhotos.Controls.Add(this.lstPhotos);
+            this.grpPhotos.Enabled = false;
+            this.grpPhotos.Location = new System.Drawing.Point(13, 64);
+            this.grpPhotos.Name = "grpPhotos";
+            this.grpPhotos.Size = new System.Drawing.Size(359, 146);
+            this.grpPhotos.TabIndex = 1;
+            this.grpPhotos.TabStop = false;
+            this.grpPhotos.Text = "&Photographs";
+            // 
+            // btnMoveUp
+            // 
+            this.btnMoveUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMoveUp.Location = new System.Drawing.Point(272, 19);
+            this.btnMoveUp.Name = "btnMoveUp";
+            this.btnMoveUp.Size = new System.Drawing.Size(75, 23);
+            this.btnMoveUp.TabIndex = 4;
+            this.btnMoveUp.Text = "Move &Up";
+            this.btnMoveUp.UseVisualStyleBackColor = true;
+            this.btnMoveUp.Click += new System.EventHandler(this.btnMoveUp_Click);
+            // 
+            // btnMoveDown
+            // 
+            this.btnMoveDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMoveDown.Location = new System.Drawing.Point(272, 48);
+            this.btnMoveDown.Name = "btnMoveDown";
+            this.btnMoveDown.Size = new System.Drawing.Size(75, 23);
+            this.btnMoveDown.TabIndex = 3;
+            this.btnMoveDown.Text = "Move &Down";
+            this.btnMoveDown.UseVisualStyleBackColor = true;
+            this.btnMoveDown.Click += new System.EventHandler(this.btnMoveDown_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRemove.Location = new System.Drawing.Point(272, 77);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "&Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnPhotoProps
+            // 
+            this.btnPhotoProps.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPhotoProps.Location = new System.Drawing.Point(272, 106);
+            this.btnPhotoProps.Name = "btnPhotoProps";
+            this.btnPhotoProps.Size = new System.Drawing.Size(75, 23);
+            this.btnPhotoProps.TabIndex = 1;
+            this.btnPhotoProps.Text = "Properti&es";
+            this.btnPhotoProps.UseVisualStyleBackColor = true;
+            this.btnPhotoProps.Click += new System.EventHandler(this.btnPhotoProps_Click);
+            // 
+            // lstPhotos
+            // 
+            this.lstPhotos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstPhotos.FormattingEnabled = true;
+            this.lstPhotos.HorizontalScrollbar = true;
+            this.lstPhotos.Location = new System.Drawing.Point(6, 19);
+            this.lstPhotos.Name = "lstPhotos";
+            this.lstPhotos.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lstPhotos.Size = new System.Drawing.Size(260, 108);
+            this.lstPhotos.TabIndex = 0;
+            this.lstPhotos.SelectedIndexChanged += new System.EventHandler(this.lstPhotos_SelectedIndexChanged);
+            this.lstPhotos.DoubleClick += new System.EventHandler(this.lstPhotos_DoubleClick);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnClose.Location = new System.Drawing.Point(154, 227);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // comboAlbums
+            // 
+            this.comboAlbums.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboAlbums.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboAlbums.DropDownWidth = 400;
+            this.comboAlbums.FormattingEnabled = true;
+            this.comboAlbums.Location = new System.Drawing.Point(6, 18);
+            this.comboAlbums.Name = "comboAlbums";
+            this.comboAlbums.Size = new System.Drawing.Size(179, 21);
+            this.comboAlbums.TabIndex = 2;
+            this.comboAlbums.SelectionChangeCommitted += new System.EventHandler(this.comboAlbums_SelectionChangeCommitted);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(191, 16);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "&Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // EditorForm
+            // 
+            this.AcceptButton = this.btnClose;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 262);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.grpPhotos);
+            this.Controls.Add(this.grpAlbums);
+            this.Name = "EditorForm";
+            this.Text = "MyAlbumEditor";
+            this.grpAlbums.ResumeLayout(false);
+            this.grpPhotos.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpAlbums;
+        private System.Windows.Forms.GroupBox grpPhotos;
+        private System.Windows.Forms.ListBox lstPhotos;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnAlbumProps;
+        private System.Windows.Forms.Button btnPhotoProps;
+        private System.Windows.Forms.Button btnMoveUp;
+        private System.Windows.Forms.Button btnMoveDown;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.ComboBox comboAlbums;
+        private System.Windows.Forms.Button btnBrowse;
+    }
+}
+
---
+++ MyAlbumEditor/EditorForm.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyPhotoAlbum;
+using MyPhotoControls;
+using System.IO;
+
+namespace MyAlbumEditor
+{
+    public partial class EditorForm : Form
+    {
+        public EditorForm()
+        {
+            InitializeComponent();
+            Manager = new AlbumManager();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            comboAlbums.DataSource = Directory.GetFiles(AlbumManager.DefaultPath, "*.abm");
+
+            OpenAlbum(comboAlbums.Text);
+
+            base.OnLoad(e);
+        }
+
+        private AlbumManager _manager;
+        private AlbumManager Manager
+        {
+            get { return _manager; }
+            set { _manager = value; }
+        }
+
+        private bool CloseAlbum()
+        {
+            if (Manager != null)
+            {
+                DialogResult result = AlbumController.AskForSave(Manager);
+                switch (result)
+                {
+                    case DialogResult.Yes:
+                        Manager.Save();
+                        break;
+                    case DialogResult.Cancel:
+                        return true;
+                }
+                Manager.Album.Dispose();
+                Manager = null;
+            }
+            return false;
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            e.Cancel = CloseAlbum();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DisplayAlbum()
+        {
+            if (Manager == null)
+            {
+                grpPhotos.Enabled = false;
+                btnAlbumProps.Enabled = false;
+                Text = "The selected album could not be opened";
+                lstPhotos.BackColor = SystemColors.Control;
+                lstPhotos.Items.Clear();
+            }
+            else
+            {
+                grpPhotos.Enabled = true;
+                btnAlbumProps.Enabled = true;
+                Text = "Album " + Manager.ShortName;
+                lstPhotos.BackColor = SystemColors.Window;
+
+                lstPhotos.BeginUpdate();
+                lstPhotos.Items.Clear();
+                foreach (Photograph p in Manager.Album)
+                {
+                    lstPhotos.Items.Add(p);
+                }
+                lstPhotos.EndUpdate();
+            }
+        }
+
+        private void btnAlbumProps_Click(object sender, EventArgs e)
+        {
+            if (Manager == null)
+                return;
+
+            using (AlbumEditDialog dlg = new AlbumEditDialog(Manager))
+            {
+                if (dlg.ShowDialog() == DialogResult.OK)
+                    DisplayAlbum();
+            }
+        }
+
+        private void btnPhotoProps_Click(object sender, EventArgs e)
+        {
+            if (Manager == null || lstPhotos.SelectedIndex < 0)
+                return; // nothing selected
+
+            Manager.Index = lstPhotos.SelectedIndex;
+
+            using (PhotoEditDialog dlg = new PhotoEditDialog(Manager))
+            {
+                if (dlg.ShowDialog() == DialogResult.OK)
+                    DisplayAlbum();
+            }
+        }
+
+        private void lstPhotos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            EnablePhotoButtons();
+        }
+
+        private void lstPhotos_DoubleClick(object sender, EventArgs e)
+        {
+            btnPhotoProps.PerformClick();
+        }
+
+        private void EnablePhotoButtons()
+        {
+            int selCount = lstPhotos.SelectedIndices.Count;
+            bool someSelected = (selCount > 0);
+
+
+            if (someSelected)
+            {
+                bool firstSelected = lstPhotos.GetSelected(0);
+                bool lastSelected = lstPhotos.GetSelected(lstPhotos.Items.Count - 1);
+                btnMoveUp.Enabled = !firstSelected;
+                btnMoveDown.Enabled = !lastSelected;
+            }
+            else
+            {
+                btnMoveUp.Enabled = false;
+                btnMoveDown.Enabled = false;
+            }
+
+            btnRemove.Enabled = someSelected;
+            btnPhotoProps.Enabled = (selCount == 1);
+        }
+
+        private void btnMoveUp_Click(object sender, EventArgs e)
+        {
+            ListBox.SelectedIndexCollection indices = lstPhotos.SelectedIndices;
+            int count = indices.Count;
+            int[] newIndices = new int[count];
+            // Move each selection up
+            for (int i = 0; i < count; i++)
+            {
+                int x = indices[i];
+                Manager.MoveItemBackward(x);
+                newIndices[i] = x - 1;
+            }
+            ReselectMovedItems(newIndices);
+        }
+
+        private void ReselectMovedItems(int[] newIndices)
+        {
+            DisplayAlbum();
+            // Reselect moved items
+            foreach (int x in newIndices)
+                lstPhotos.SetSelected(x, true);
+        }
+
+        private void btnMoveDown_Click(object sender, EventArgs e)
+        {
+            ListBox.SelectedIndexCollection indices = lstPhotos.SelectedIndices;
+            int count = indices.Count;
+            int[] newIndices = new int[count];
+            // Move each selection down
+            for (int i = count - 1; i >= 0; i--)
+            {
+                int x = indices[i];
+                Manager.MoveItemForward(x);
+                newIndices[i] = x + 1;
+            }
+            ReselectMovedItems(newIndices);
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            ListBox.SelectedIndexCollection indices = lstPhotos.SelectedIndices;
+            int count = indices.Count;
+            string msg;
+
+            if (count == 1)
+                msg = "Do you really want to remove the selected photograph?";
+            else
+                msg = String.Format("Do you really want to remove the {0} selected photographs?", count);
+
+            DialogResult result = MessageBox.Show(msg, "Remove Photos?", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+
+            if (result == DialogResult.Yes)
+            {
+                for (int i = count - 1; i >= 0; i--)
+                    Manager.Album.RemoveAt(indices[i]);
+                DisplayAlbum();
+            }
+        }
+
+        private void comboAlbums_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            string path = comboAlbums.Text;
+            // Don't reopen the existing album
+            if (Manager != null && path == Manager.FullName)
+                return;
+            // Open the new album
+            OpenAlbum(path);
+        }
+
+        private void OpenAlbum(string path)
+        {
+            string password = null;
+            if (path != null && path.Length > 0 && AlbumController.CheckAlbumPassword(path, ref password))
+            {
+                if (CloseAlbum())
+                    return; // cancel open
+
+                try
+                {
+                    Manager = new AlbumManager(
+                    path, password);
+                }
+                catch (AlbumStorageException)
+                {
+                    Manager = null;
+                }
+            }
+            DisplayAlbum();
+            EnablePhotoButtons();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dlg = new FolderBrowserDialog())
+            {
+                dlg.Description = "Select an album file directory to add to the dialog.";
+                dlg.SelectedPath = AlbumManager.DefaultPath;
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    if (CloseAlbum())
+                        return; // cancel browse
+
+                    comboAlbums.Text = null;
+                    comboAlbums.DataSource = Directory.GetFiles(dlg.SelectedPath, "*.abm");
+                    OpenAlbum(comboAlbums.Text);
+                }
+            }
+        }
+    }
+}
